2ccdb0593a452c8c6b9f29484525b7f9ecbc565b
Mar1owe <Marlowe.xzk@gmail.com>
2023-11-03 10:23:59 +0800

    update

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-k = 1
-def hailstone(n):
-    if n == 1:
-        k += 1
-        print(k)
-        return 1
-    elif n%2 ==0:
-        k += 1
-        print(n/2)
-        return hailstone(n/2)
+def merge(n1, n2):
+    """Merges two numbers by digit in decreasing order.
+    >>> merge(31, 42)
+    4321
+    >>> merge(21, 0)
+    21
+    >>> merge (21, 31) 
+    3211
+    """
+    if n1 == 0:
+        return n2
+    elif n2 == 0:
+        return n1
+    elif n1 % 10 < n2 % 10:
+        return merge(n1 // 10, n2) * 10 + n1 % 10
     else:
-        k += 1
-        print(3*n + 1)
-        return hailstone(3*n + 1)
+        return merge(n1, n2 // 10) * 10 + n2 % 10
