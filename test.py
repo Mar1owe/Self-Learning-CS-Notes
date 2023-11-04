25e19099043f584098c3fc9bec6ab20e9f0760ff
Mar1owe <Marlowe.xzk@gmail.com>
2023-11-04 10:12:34 +0800

    homework 02

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-def merge(n1, n2):
-    """Merges two numbers by digit in decreasing order.
-    >>> merge(31, 42)
-    4321
-    >>> merge(21, 0)
-    21
-    >>> merge (21, 31) 
-    3211
-    """
-    if n1 == 0:
-        return n2
-    elif n2 == 0:
-        return n1
-    elif n1 % 10 < n2 % 10:
-        return merge(n1 // 10, n2) * 10 + n1 % 10
+def factorial_rec(n):
+    if n == 1:
+        return 1
     else:
-        return merge(n1, n2 // 10) * 10 + n2 % 10
+        return n * factorial_rec(n-1)
